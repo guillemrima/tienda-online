b5baffb719d859fb67d74f4f13050edac2bf9f76
guillemrima <rivasmartorellguillem@gmail.com>
2023-03-24 09:27:05 +0100

    Fix: Reestructurar scripts

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
+//SECTION FAQ FUNCTIONALITY
 const buttonsQuestion = document.querySelectorAll(".button-question")
 const answerFAQ = document.querySelectorAll(".faq-answer")
-const featuredSection = document.getElementById("featured")
-const detailsSection = document.getElementById("details")
-const faqSection = document.getElementById("faq")
-const teamSection = document.getElementById("team")
-const contactSection = document.getElementById("contact")
-const footerSection = document.getElementById("footer")
-const checkoutButtonHeader = document.getElementById("checkoutButtonHeader");
-const checkoutButtonSlider = document.getElementById("checkoutButtonSlider");
-const checkoutButtonCheckoutBox = document.getElementById("checkoutButtonCheckoutBox")
-const checkoutBox = document.getElementById("checkout")
-const closeCheckoutBox = document.getElementById("closeButton")
-const detailsCheckout = document.getElementById("detailsCheckout")
-const backButtonCheckoutPart2 = document.getElementById("backButtonCheckoutPart2")
-const checkoutPart1 = document.getElementById("checkoutPart1")
-const checkoutPart2 = document.getElementById("checkoutPart2")
-const imageCheckout = document.getElementById("imagenCheckout")
-const buttonFront = document.getElementById("buttonFront")
-const frontContact = document.getElementById("frontContact")
-const backContact = document.getElementById("backContact")
-const buttonBack = document.getElementById("buttonBack")
-
 function showAnswer(event) {
     for (let i = 0; i < answerFAQ.length; i++){
         if (buttonsQuestion[i].getAttribute("value") === "false") {
@@ -41,7 +21,23 @@ function showAnswer(event) {
     }
     }
 }
+buttonsQuestion.forEach(buttonQuestion => {
+    buttonQuestion.addEventListener("click", showAnswer)
+})
 
+
+
+
+//SECTION MODAL FUNCTIONALITY
+const checkoutButtonHeader = document.getElementById("checkoutButtonHeader");
+const checkoutButtonSlider = document.getElementById("checkoutButtonSlider");
+const checkoutButtonCheckoutBox = document.getElementById("checkoutButtonCheckoutBox")
+const checkoutBox = document.getElementById("checkout")
+const closeCheckoutBox = document.getElementById("closeButton")
+const detailsCheckout = document.getElementById("detailsCheckout")
+const backButtonCheckoutPart2 = document.getElementById("backButtonCheckoutPart2")
+const checkoutPart1 = document.getElementById("checkoutPart1")
+const checkoutPart2 = document.getElementById("checkoutPart2")
 function showCheckoutBox() {
     checkoutBox.style.display = "flex"
     checkoutBox.style.animation = "fadeIn 0.2s ease-in"
@@ -56,7 +52,6 @@ function hideCheckoutButton() {
     checkoutBox.style.zIndex = "-999"
     detailsCheckout.style.animation = ""
 }
-
 function showCheckoutPart1() {
     detailsCheckout.style.left = "50%"
     detailsCheckout.style.animation = "fadeCheckoutPart2 0.7s ease-in-out forwards"
@@ -64,42 +59,40 @@ function showCheckoutPart1() {
     checkoutPart2.style.animation = "fadeOut 0.5s ease-in forwards"
 
 }
-
 function showCheckoutPart2() {
     detailsCheckout.style.left = "0%"
     detailsCheckout.style.animation = "fadeCheckoutPart1 0.7s ease-in-out forwards"
     checkoutPart2.style.animation = "fadeIn 0.5s ease-in"
     checkoutPart1.style.animation = "fadeOut 0.5s ease-in forwards"
 }
-
-function flipCardBack() {
-    frontContact.style.transform ="perspective(1000px) rotateY(180deg)"
-    backContact.style.transform = "perspective(1000px) rotateY(360deg)"
-}
-
-function flipCardFront() {
-    frontContact.style.transform ="perspective(1000px) rotateY(0deg)"
-    backContact.style.transform = "perspective(1000px) rotateY(180deg)"
-}
-
-buttonsQuestion.forEach(buttonQuestion => {
-    buttonQuestion.addEventListener("click", showAnswer)
-})
-
-// ES UN POCO CUTRILLO PERO FUNCIONA
-
-function showSection() {
-    if(window.scrollY > 600 && window.scrollY < 1000){
-    featuredSection.style.animation = "fadeIn 1s ease-in forwards"}
-    else if(window.scrollY > 1000){
-        detailsSection.style.animation = "fadeIn 1s ease-in forwards"}
-}
-
-window.addEventListener("scroll", showSection)
 checkoutButtonHeader.addEventListener("click", showCheckoutBox)
 checkoutButtonSlider.addEventListener("click", showCheckoutBox)
 checkoutButtonCheckoutBox.addEventListener("click", showCheckoutPart2)
 backButtonCheckoutPart2.addEventListener("click", showCheckoutPart1)
 closeCheckoutBox.addEventListener("click", hideCheckoutButton)
+
+
+
+
+
+
+//SECTION CONTACT FUNCTIONALITY
+const buttonFront = document.getElementById("buttonFront")
+const frontContact = document.getElementById("frontContact")
+const backContact = document.getElementById("backContact")
+const buttonBack = document.getElementById("buttonBack")
+function flipCardBack() {
+    frontContact.style.transform ="perspective(1000px) rotateY(180deg)"
+    backContact.style.transform = "perspective(1000px) rotateY(360deg)"
+}
+function flipCardFront() {
+    frontContact.style.transform ="perspective(1000px) rotateY(0deg)"
+    backContact.style.transform = "perspective(1000px) rotateY(180deg)"
+}
 buttonFront.addEventListener("click", flipCardBack)
 buttonBack.addEventListener("click", flipCardFront)
+
+
+
+
+
