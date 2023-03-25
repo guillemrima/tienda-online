a035af06ddc6d6a6a3d55f72a4ec0dd7b305f302
guillemrima <117720260+guillemrima@users.noreply.github.com>
2023-03-25 11:45:42 +0100

    Fix: Refactor Modal interactions

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 import './flip-element.js';
-import {openModal, closeModal} from'./modal.js';
+import {openModal, closeModal, setCheckoutSectionModal, quitCheckoutSectionModal,} from'./modal.js';
 import './social-newtowk.js';
 import './swipeable-element.js';
 
 
 openModal();
 closeModal();
+setCheckoutSectionModal();
+quitCheckoutSectionModal();
 
--- js/modal.js
+++ js/modal.js
@@ -1,21 +1,56 @@
 export const openModal = () => {
     const modalButtonsOpen = document.querySelectorAll(".button-show-modal");
-    const modalCheckout = document.querySelector("#checkout");
+    const modal = document.querySelector("#modal");
 
     modalButtonsOpen.forEach((modalButtonOpen) => {
         modalButtonOpen.addEventListener("click", () => {
-            modalCheckout.classList.add("active")
-            console.log("Prueba modal_1");
+            modal.classList.add("active")
         })
     })
 }
 
 export const closeModal = () => {
-    const modalCheckout = document.querySelector("#checkout"); 
+    const modal = document.querySelector("#modal"); 
     const modalButtonClose = document.querySelector(".button-close-modal");
+    const modalInformation = document.querySelector("#detailsModal");
 
     modalButtonClose.addEventListener("click",  () => {
-        modalCheckout.classList.remove("active")
-        console.log("Prueba modal_2");
+        modal.classList.remove("active")
+        modalInformation.classList.remove("active")
+        modalPart2.classList.remove("fadeIn");
+        modalPart1.classList.remove("fadeOut")
+        modalPart2.classList.remove("fadeOut");
+        modalPart1.classList.remove("fadeIn")
+    })
+}
+
+export const setCheckoutSectionModal = () => {
+    const modalInformation = document.querySelector("#detailsModal");
+    const checkoutButton = document.querySelector("#buttonToCheckoutModal")
+    const modalPart1 = document.querySelector("#modalPart1");
+    const modalPart2 = document.querySelector("#modalPart2");
+
+    checkoutButton.addEventListener("click", () => {
+        modalInformation.classList.add("active");
+        modalPart2.classList.add("fadeIn");
+        modalPart1.classList.add("fadeOut")
+        modalPart2.classList.remove("fadeOut");
+        modalPart1.classList.remove("fadeIn")
+    })
+}
+
+export const quitCheckoutSectionModal = () => {
+    const modalInformation = document.querySelector("#detailsModal");
+    const closeButton = document.querySelector("#buttonToDetailsModal")
+    const modalPart1 = document.querySelector("#modalPart1");
+    const modalPart2 = document.querySelector("#modalPart2");
+
+
+    closeButton.addEventListener("click", () => {
+        modalInformation.classList.remove("active");
+        modalPart2.classList.add("fadeOut");
+        modalPart1.classList.add("fadeIn")
+        modalPart2.classList.remove("fadeIn");
+        modalPart1.classList.remove("fadeOut")
     })
 }
