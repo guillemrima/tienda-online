e4a266ce2c9c6287de1e92695c0fcdd3f30db14d
guillemrima <rivasmartorellguillem@gmail.com>
2023-06-27 14:57:35 +0200

    ENV

--- api/index.js
+++ api/index.js
@@ -3,9 +3,11 @@ const cors = require("cors");
 const fs = require('fs'); 
 const app = express();
 const multer = require('multer');
+const dotenv = require('dotenv').config()
+const process = require('process')
 
 var corsOptions = {
-    origin: ['http://localhost:8081', 'http://127.0.0.1:5500', 'http://127.0.0.1:5501']
+    origin: [process.env.BASE_URL, 'http://localhost:8081', 'http://127.0.0.1:5500', 'http://127.0.0.1:5501']
 };
 
 app.use(cors(corsOptions));
