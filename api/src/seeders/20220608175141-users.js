ff719c2f3a020e395b9bcee608ab71e8d99f033d
guillemrima <rivasmartorellguillem@gmail.com>
2023-07-14 12:41:49 +0200

    Feat: Create a new migracion

---
+++ api/src/seeders/20220608175141-users.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('users', [
+      {
+        id: 1,
+        name: 'guillem',
+        email: 'rivasmartorellguillem@gmail.com',
+        password: '123.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('pages', null, {});
+  }
+};
